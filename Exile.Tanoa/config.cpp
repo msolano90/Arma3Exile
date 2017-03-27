3bb689b77e5227436504d74b1d2884f13d99ecff
KlownGT <gtabor102@live.com>
2017-03-26 18:42:38 -0700

    Update config.cpp

--- Exile.Tanoa/config.cpp
+++ Exile.Tanoa/config.cpp
@@ -2722,6 +2722,8 @@ class CfgExileArsenal
 
 class CfgExileCustomCode 
 {
+	ExileClient_gui_xm8_slide = "ExAdClient\XM8\CustomCode\ExileClient_gui_xm8_slide.sqf";
+	ExileClient_gui_xm8_show = "ExAdClient\XM8\CustomCode\ExileClient_gui_xm8_show.sqf";
 	/*
 		You can overwrite every single file of our code without touching it.
 		To do that, add the function name you want to overwrite plus the 
@@ -6325,3 +6327,76 @@ class CfgVehicleTransport
 		};
 	};
 };
+
+/////ExAD////
+
+class CfgXM8
+{
+	extraApps[] = {/*"ExAd_VG","ExAd_Info",*/"ExAd_CHVD",/*"ExAd_Journal","ExAd_Bike","ExAd_Quad",*/"ExAd_SB"};
+	
+	//class ExAd_VG 
+	{
+		title = "Virtual Garage";
+		controlID = 50000;					//IDC:50000 -> 50015 || These need to be unique and out of range from each other 
+		logo = "ExadClient\XM8\Apps\VG\Icon_VG.paa";
+		onLoad = "ExAdClient\XM8\Apps\VG\onLoad.sqf";
+		onOpen = "ExAdClient\XM8\Apps\VG\onOpen.sqf";
+		onClose = "ExAdClient\XM8\Apps\VG\onClose.sqf";
+	};	
+	//class ExAd_Info 
+	{
+		title = "Server Info";
+		controlID = 50100;					//IDC:50100 -> 50102 || These need to be unique and out of range from each other
+		logo = "ExadClient\XM8\Apps\Info\Icon_SI.paa";
+		onLoad = "ExAdClient\XM8\Apps\Info\onLoad.sqf";
+		onOpen = "ExAdClient\XM8\Apps\Info\onOpen.sqf";
+		onClose = "ExAdClient\XM8\Apps\Info\onClose.sqf";
+	};	
+	class ExAd_CHVD 
+	{
+		title = "View Distance Settings";
+		controlID = 50200;					//IDC:50200 -> 50102 || These need to be unique and out of range from each other
+		config = "ExadClient\XM8\Apps\CHVD\config.sqf";
+		logo = "ExadClient\XM8\Apps\CHVD\Icon_CHVD.paa";
+		onLoad = "ExAdClient\XM8\Apps\CHVD\onLoad.sqf";
+		onOpen = "ExAdClient\XM8\Apps\CHVD\onOpen.sqf";
+		onClose = "ExAdClient\XM8\Apps\CHVD\onClose.sqf";
+	};		
+	//class ExAd_Journal 
+	{
+		title = "Journal";
+		controlID = 50300;					//IDC:50300 -> 50305 || These need to be unique and out of range from each other
+		config = "ExadClient\XM8\Apps\Journal\config.sqf";
+		logo = "ExadClient\XM8\Apps\Journal\Icon_Journal.paa";
+		onLoad = "ExAdClient\XM8\Apps\Journal\onLoad.sqf";
+		onOpen = "ExAdClient\XM8\Apps\Journal\onOpen.sqf";
+		onClose = "ExAdClient\XM8\Apps\Journal\onClose.sqf";
+	};
+	//class ExAd_Bike
+	{
+		title = "Deploy Bike";
+		config = "ExadClient\XM8\Apps\DeployVehicle\config.sqf";
+		bambiState = 0;
+		vehicleClass = "Exile_Bike_MountainBike";
+		recipe[] = {{"Exile_Item_ExtensionCord",-1}};
+		packable = 1;
+		autoCleanUp = 1;
+		quickFunction = "['ExAd_Bike'] call ExAd_XM8_DV_fnc_spawnVehicle";
+	};
+	//class ExAd_Quad
+	{
+		title = "Deploy Quad";
+		bambiState = 0;
+		vehicleClass = "Exile_Bike_QuadBike_Fia";
+		recipe[] = {{"Exile_Item_ExtensionCord",1}};
+		packable = 1;
+		quickFunction = "['ExAd_Quad'] call ExAd_XM8_DV_fnc_spawnVehicle";
+	};
+	class ExAd_SB 
+	{
+		title = "Statsbar Settings";
+		controlID = 50400;					//IDC:50400 -> 50475 || These need to be unique and out of range from each other
+		logo = "ExadClient\XM8\Apps\SB_Settings\Icon_SB.paa";
+		onLoad = "ExAdClient\XM8\Apps\SB_Settings\onLoad.sqf";
+	};
+}; 
